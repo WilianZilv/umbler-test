a78d6ed01cbc56fe182c9cab4c1c1a583a0b7dd8
Wilian <wilianzilv@gmail.com>
2020-06-26 09:31:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcrypt')
+const express = require('express')
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    const hash = await bcrypt.hash(Date.now().toString(), 1)
+    res.json({ hash })
+})
+
+app.listen(process.env.PORT || 80, () => console.log('Funcionando!'))

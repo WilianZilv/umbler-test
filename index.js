912f89b706fc496fb62b118937566cb5c7f4218e
Wilian <wilianzilv@gmail.com>
2020-06-26 10:42:29 -0300

    adicionado node_modules

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const bcrypt = require('bcrypt')
-const express = require('express')
+const bcrypt = require('bcrypt');
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/', async (req, res) => {
-    const hash = await bcrypt.hash(Date.now().toString(), 1)
-    res.json({ hash })
-})
+  const hash = await bcrypt.hash(Date.now().toString(), 1);
+  res.json({ hash });
+});
 
-app.listen(process.env.PORT || 80, () => console.log('Funcionando!'))
+app.listen(process.env.PORT || 80, () => console.log('Funcionando!'));
